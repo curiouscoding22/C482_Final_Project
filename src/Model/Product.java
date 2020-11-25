fc019e02e465075b5c6c619a45077a6f9ef84f09
curiouscoding22 <lambertbe22@gmail.com>
2020-11-25 15:21:59 -0500

    Javadocs added for Product class

--- src/Model/Product.java
+++ src/Model/Product.java
@@ -13,8 +13,20 @@ public class Product {
     private int max;
     private ObservableList<Part> associatedParts = FXCollections.observableArrayList();
 
+    /**
+     * This is the empty constructor. It is used to create an instance of the product class.
+     */
     public Product(){}
 
+    /**
+     * This is the full constructor. This is used to create full Product objects.
+     * @param productID
+     * @param productName
+     * @param productPrice
+     * @param productStock
+     * @param min
+     * @param max
+     */
     public Product(int productID, String productName, double productPrice, int productStock, int min, int max){
         this.productID = productID;
         this.productName = productName;
@@ -24,63 +36,124 @@ public class Product {
         this.max = max;
     }
 
-    public int getProductID() {
+    /**
+     * This method returns the product's ID number.
+     * @return the ID number.
+     */
+    public int getID() {
         return productID;
     }
 
-    public void setProductID(int productID) {
+    /**
+     * This method sets the product ID number.
+     * @param productID
+     */
+    public void setID(int productID) {
         this.productID = productID;
     }
 
-    public String getProductName() {
+    /**
+     * This method returns the product's name.
+     * @return the name
+     */
+    public String getName() {
         return productName;
     }
 
-    public void setProductName(String productName) {
+    /**
+     * This method sets the product name
+     * @param productName
+     */
+    public void setName(String productName) {
         this.productName = productName;
     }
 
-    public double getProductPrice() {
+    /**
+     *This method returns the product price.
+     * @return the price
+     */
+    public double getPrice() {
         return productPrice;
     }
 
-    public void setProductPrice(double productPrice) {
+    /**
+     *This method sets the product price.
+     * @param productPrice
+     */
+    public void setPrice(double productPrice) {
         this.productPrice = productPrice;
     }
 
-    public int getProductStock() {
+    /**
+     *This method returns the inventory count
+     * @return the stock
+     */
+    public int getStock() {
         return productStock;
     }
 
-    public void setProductStock(int productStock) {
+    /**
+     *This method sets the product stock level.
+     * @param productStock
+     */
+    public void setStock(int productStock) {
         this.productStock = productStock;
     }
 
+    /**
+     *This method returns the minimum stock level,
+     * @return the minimum level.
+     */
     public int getMin() {
         return min;
     }
 
+    /**
+     *This method sets the minimum stock level.
+     * @param min
+     */
     public void setMin(int min) {
         this.min = min;
     }
 
+    /**
+     *This method returns the maximum stock level.
+     * @return the maximum stock
+     */
     public int getMax() {
         return max;
     }
 
+    /**
+     *This method sets the maximum stock level.
+     * @param max
+     */
     public void setMax(int max) {
         this.max = max;
     }
 
+    /**
+     *This method adds a part to the associated part list.
+     * @param part
+     */
     public void addAssociatedPart(Part part){
         associatedParts.add(part);
     }
 
+    /**
+     *This method removes a part from the associated part list.
+     * @param selectedAssociatePart
+     * @return true
+     */
     public boolean deleteAssociatedPart(Part selectedAssociatePart){
         associatedParts.remove(selectedAssociatePart);
         return true;
     }
 
+    /**
+     *This method returns the list of all associated parts.
+     * @return all associated parts
+     */
     public ObservableList<Part> getAllAssociatedParts(){
         return associatedParts;
     }
