ff29deb29ec5de0a6f0657c53cfb9eaa5d96c456
curiouscoding22 <lambertbe22@gmail.com>
2020-11-26 10:03:37 -0500

    Javadocs added for AddProductForm controller.

--- src/Controller/AddProductForm.java
+++ src/Controller/AddProductForm.java
@@ -9,7 +9,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
-
 import javax.xml.bind.ValidationException;
 import java.net.URL;
 import java.util.Random;
@@ -44,12 +43,16 @@ public class AddProductForm implements Initializable {
     private boolean isMatch;
     private ObservableList<Part> partsToAssociate = FXCollections.observableArrayList();
 
+    /**
+     * This is the Product ID method. This method generates a random product number and checks the existing product numbers to confirm there are no duplicates.
+     * @return the product ID.
+     */
     private int assignProductID(){
         int randProdID;
         randProdID = 1 + rand.nextInt(999999);
 
         for(Product p : Inventory.getAllProducts()){
-            if(p.getProductID() == randProdID){
+            if(p.getID() == randProdID){
                 isMatch = true;
                 assignProductID();
             }
@@ -57,6 +60,10 @@ public class AddProductForm implements Initializable {
         return randProdID;
     }
 
+    /**
+     *This is the add part method. When the user clicks the add button, the part that is selected in the part table is added to the associated part list and displayed in the bottom table.
+     * @param actionEvent the click on the add button.
+     */
     public void addAssociatedPart(ActionEvent actionEvent) {
         Part selectPart = productPartTable.getSelectionModel().getSelectedItem();
         if(selectPart != null){
@@ -71,6 +78,10 @@ public class AddProductForm implements Initializable {
         }
     }
 
+    /**
+     *This is the part search method. This method searches the part table by part name or ID number for a specified part when the search button is clicked.
+     * @param actionEvent the click on the search button.
+     */
     public void productPartSearch(ActionEvent actionEvent) {
         String searchText = productPartSearchField.getText();
         if(searchText.isEmpty()){
@@ -92,6 +103,10 @@ public class AddProductForm implements Initializable {
         productPartSearchField.setText("");
     }
 
+    /**
+     *This is the remove associated part method. This method removes an associated part from the lower table and the associated parts list.
+     * @param actionEvent the click on the Remove Associated button.
+     */
     @FXML
     public void removeAssociatedPart(ActionEvent actionEvent) {
         Part p = associatedPartTable.getSelectionModel().getSelectedItem();
@@ -100,6 +115,13 @@ public class AddProductForm implements Initializable {
         }
     }
 
+    /**
+     *This is the save method. This method takes the information entered in the text fields and converts it to the specified data types to create a product object that is added to the Inventory.
+     * @param actionEvent the click on the save button.
+     * @throws NumberFormatException
+     * @throws NullPointerException
+     * @throws ValidationException
+     */
     @FXML
     public void saveNewProduct(ActionEvent actionEvent) throws NumberFormatException, NullPointerException, ValidationException {
         int newProductID = Integer.parseInt(productIDField.getText());
@@ -110,10 +132,10 @@ public class AddProductForm implements Initializable {
         int newProductMin = Integer.parseInt(productMinField.getText());
 
         Product newProduct = new Product();
-        newProduct.setProductID(newProductID);
-        newProduct.setProductName(newProductName);
+        newProduct.setID(newProductID);
+        newProduct.setName(newProductName);
         try {
-            newProduct.setProductStock(newProductInv);
+            newProduct.setStock(newProductInv);
         } catch (NumberFormatException ex) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Input Error");
@@ -127,7 +149,7 @@ public class AddProductForm implements Initializable {
             alert.showAndWait();
         }
         try {
-            newProduct.setProductPrice(newProductPrice);
+            newProduct.setPrice(newProductPrice);
         } catch (NumberFormatException ex) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Input Error");
@@ -179,7 +201,12 @@ public class AddProductForm implements Initializable {
 
     }
 
-    @FXML public void cancelAddNewProduct(ActionEvent actionEvent) {
+    /**
+     *This is the cancel method. When the user clicks the cancel button, this method confirms the action then closes the stage without saving a new part.
+     * @param actionEvent the click on the cancel button.
+     */
+    @FXML
+    public void cancelAddNewProduct(ActionEvent actionEvent) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Cancel Add Product");
         alert.setContentText("Are you sure you want to cancel?");
@@ -191,6 +218,12 @@ public class AddProductForm implements Initializable {
         });
     }
 
+    /**
+     *This is the validation method. This method reviews the entered information in each field and confirms it is valid. If it isn't the method displays a descriptive error method to prompt the user to correct it.
+     * @param product the produce which is being checked.
+     * @return true.
+     * @throws ValidationException
+     */
     public boolean isValidProduct(Product product) throws ValidationException{
 
         String checkedName = productNameField.getText();
